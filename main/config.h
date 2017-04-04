7278f8c1e4569f3636d4cf889a2bc55b415dcf78
Jake Riffle <jvriffle@gmail.com>
2017-04-04 13:23:26 -0400

    Set up a new repository with functioning autotools.

---
+++ main/config.h
@@ -0,0 +1,43 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to dummy `main' function (if any) required to link to the Fortran
+   libraries. */
+/* #undef F77_DUMMY_MAIN */
+
+/* Define to a macro mangling the given C identifier (in lower and upper
+   case), which must not contain underscores, for linking with Fortran. */
+#define F77_FUNC(name,NAME) name ## _
+
+/* As F77_FUNC, but for C identifiers containing underscores. */
+#define F77_FUNC_(name,NAME) name ## _
+
+/* Define if F77 and FC dummy `main' functions are identical. */
+/* #undef FC_DUMMY_MAIN_EQ_F77 */
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "linear_algebra"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "linear_algebra"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "linear_algebra 0.01"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "linear_algebra"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.01"
+
+/* Version number of package */
+#define VERSION "0.01"
---
+++ main/test_kdv_equation.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int
+main(int argc, char **argv)
+{
+  printf("Autotools setup -- successful\n");
+  return 0;
+}
